490451d4b5d7c9694082e3249c6353158a4ec3ea
Davi Silva <davi.psilva@midway.com.br>
2024-11-15 16:18:25 -0300

    rename entrypoint

--- src/main/java/dev/dsilva/workshopspringmongo/WorkshopSpringMongoApplication.java
+++ src/main/java/dev/dsilva/workshopspringmongo/WorkshopSpringMongoApplication.java
@@ -4,10 +4,9 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class WorkshopspringmongoApplication {
+public class WorkshopSpringMongoApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(WorkshopspringmongoApplication.class, args);
+		SpringApplication.run(WorkshopSpringMongoApplication.class, args);
 	}
-
 }
