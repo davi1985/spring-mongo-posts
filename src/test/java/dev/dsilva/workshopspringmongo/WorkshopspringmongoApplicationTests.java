75fea41890f36f8e314885bda4aee51ee0556df9
Davi Silva <davi.psilva@midway.com.br>
2024-11-15 16:16:21 -0300

    initial files

---
+++ src/test/java/dev/dsilva/workshopspringmongo/WorkshopspringmongoApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.dsilva.workshopspringmongo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WorkshopspringmongoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
